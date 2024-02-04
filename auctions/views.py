5f3f7e0ddfaba5c861d55cf3326f91ed67f99f7b
duckraper <klyman047@gmail.com>
2024-02-03 23:18:01 -0500

    arregladas varias templates e implementado lo correspondiente a la watchlist

--- auctions/views.py
+++ auctions/views.py
@@ -180,9 +180,10 @@ def show_listing(request, listing_id):
         return render(request, 'auctions/error.html', {
             'message': 'Listing does not exist.',
         })
-
+    
     return render(request, 'auctions/listing.html', {
         'listing': listing,
+        'watchlist': request.user.watchlist.all(),
     })
 
 
@@ -222,6 +223,32 @@ def show_watchlist(request):
     })
 
 
+def add_watchlist(request, listing_id):
+    try:
+        listing = AuctionListing.objects.get(pk=listing_id)
+    except AuctionListing.DoesNotExist:
+        return render(request, 'auctions/error.html', {
+            'message': 'Listing does not exists.'
+        })
+
+    request.user.watchlist.add(listing)
+
+    return HttpResponseRedirect(reverse('listing', args=(listing.pk,)))
+
+def remove_watchlist(request, listing_id):
+    try:
+        listing = AuctionListing.objects.get(pk=listing_id)
+    except AuctionListing.DoesNotExist:
+        return render(request, 'auctions/error.html', {
+            'message': 'Listing does not exists.'
+        })
+
+    request.user.watchlist.remove(listing)
+
+    return HttpResponseRedirect(reverse('listing', args=(listing.pk,)))
+
+
+
 @login_required(login_url='login')
 def show_my_listings(request):
     return render(request, 'auctions/my_listings.html', {
@@ -248,33 +275,38 @@ def bid(request, listing_id):
         listing = AuctionListing.objects.get(pk=listing_id)
     except AuctionListing.DoesNotExist:
         return render(request, 'auctions/error.html', {
-            'message': 'Listing does not exist.',
+            'message': 'Listing does not exists.',
         })
 
     if request.method == "POST":
         bid_price = float(request.POST['bid'])
-        user = request.user
-
-        bid = Bid.objects.create(
-            user=request.user,
-            auction_listing=listing,
-            price=bid_price,
-        )
 
         if bid_price <= listing.current_price:  # type: ignore
             return render(request, 'auctions/listing.html', {
                 'message': 'Price must be greater than the current price.',
             })
 
-        listing.current_price = bid_price  # type: ignore
+        bid = Bid.objects.create(
+            user=request.user,
+            auction_listing=listing,
+            price=bid_price,
+        )
+        bid.save()
+        Bid.objects.filter(auction_listing=listing).exclude(
+            pk=bid.pk).update(state='O')
+        request.user.bids.all().get(pk=bid.pk).state = 'W'
+        listing.bids.all().get(pk=bid.pk).state = "W"
+
+        listing.current_price = bid.price
         listing.save()
         return HttpResponseRedirect(reverse('listing', args=(listing.pk,)))
 
     return render(request, 'auctions/listing.html', {
         'listing': listing,
+        'watchlist': request.user.watchlist.all(),
     })
 
-# TODO Revisar bugs
+
 def close(request, listing_id):
     try:
         listing = AuctionListing.objects.get(pk=listing_id)
@@ -282,13 +314,17 @@ def close(request, listing_id):
         return render(request, 'auctions/error.html', {
             'message': 'Listing does not exist.',
         })
+
     highest_bid = listing.bids.all().order_by('-price').first()  # type: ignore
     user = highest_bid.user
+    print(user)
 
     highest_bid.state = 'F'  # ganador
+    # user.bids.all().get(pk=highest_bid.pk).state = 'F'
     listing.bids.all().exclude(pk=highest_bid.pk).update(state='L')  # perdedores
-
+    
     listing.opened = False
+    highest_bid.save()
     listing.save()
     return HttpResponseRedirect(reverse('index'))
 
